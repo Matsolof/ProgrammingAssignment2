b24b8f1b2dda6b31e57f12b9487f216479d6ce21
Matsolof <matso.olsson@telia.com>
2014-12-17 11:29:02 +0100

    Ass 2
    Cashing

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+     inv <- NULL
+     set <- function(y) {
+          x <<- y
+          inv<<- NULL
+     }
+     get <- function() x
+     setinv <- function(solve) inv <<- solve
+     getinv<- function() inv
+     list(set = set, get = get,
+          setinv = setinv,
+          getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<- function(x, ...) {
+     inv <- x$getinv()
+     if(!is.null(inv)) {
+          message("getting cached data")
+          return(inv)
+     }
+     data <- x$get()
+     inv <- solve(data, ...)
+     x$setinv(inv)
+     inv
 }
+
+amatrix<-makeCacheMatrix(matrix(c(1,2,3,4),nrow=2, ncol=2))
+
+amatrix$get()
+
+cacheSolve(amatrix)
